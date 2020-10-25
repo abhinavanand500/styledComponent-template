7c73b881125eef9e5c212e2c2f6ab5f4a83b24bb
Abhinav Anand <abhinav.anand500@gmail.com>
2020-10-26 00:21:39 +0530

    navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { FaTimes, FaBars } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+import { NavLink } from "react-router-dom";
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    NavIcon,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks,
+} from "./Navbar.element";
+
+const Navbar = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    return (
+        <>
+            <IconContext.Provider value={{ color: "#fff" }}>
+                <Nav>
+                    <NavbarContainer>
+                        <NavLogo to='/'>
+                            <NavIcon />
+                            ABHINAV
+                        </NavLogo>
+                        <MobileIcon onClick={handleClick}>
+                            {click ? <FaTimes /> : <FaBars />}
+                        </MobileIcon>
+                        <NavMenu onClick={handleClick} click={click}>
+                            <NavItem>
+                                <NavLinks to='/'>Home</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to='/'>Services</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to='/'>Projects</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to='/'>Contact</NavLinks>
+                            </NavItem>
+                        </NavMenu>
+                    </NavbarContainer>
+                </Nav>
+            </IconContext.Provider>
+        </>
+    );
+};
+
+export default Navbar;
